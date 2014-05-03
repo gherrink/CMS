738d89d62130cc7612b97e4b8f08c210edd86c60
Maurice Busch <busch.maurice@gmx.net>
2014-05-03 17:10:25 +0200

    .travis.yml changed 12

---
+++ protected/tests/unit/MsgPickerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * @author Maurice Busch <busch.maurice@gmx.net>
+ * @copyright 2014
+ * @version 0.1
+ *
+ */
+
+class MsgPickerTest extends CTestCase
+{
+	public function testGetMessage()
+	{
+		
+	}
+}

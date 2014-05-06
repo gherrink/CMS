a485ef2cf6a756a6e7430b5fa546cd1bdeb53f90
Maurice Busch <busch.maurice@gmx.net>
2014-05-06 19:10:56 +0200

    Configs changed

--- index-test.php
+++ index-test.php
@@ -4,9 +4,12 @@
  * This file should be removed when the application is deployed for production.
  */
 
+if(! defined(TEST_CONFIG))
+	define('TEST_CONFIG', 'test.php');
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/test.php';
+$config=dirname(__FILE__).'/protected/config/'.TEST_CONFIG;
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);

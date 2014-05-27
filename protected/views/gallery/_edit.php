f9c677219da282ae29b38ce331fa74699f47b560
immogen <alessio93bisgen@gmail.com>
2014-05-27 11:24:43 +0200

    Galerieübersicht-Datenbankanbindung geschafft; button zum Erstellen einer Galerie eingefügt und mit Funktion versehen

--- protected/views/gallery/_edit.php
+++ protected/views/gallery/_edit.php
@@ -11,17 +11,6 @@
  * @var String $url
  */
 
-$roles = SelectHelper::getSiteRoles();
-$languages = SelectHelper::getActiveLanguages();
-
-$selectedRole = MSG::MSITE;
-if($model->roleaccess !== null)
-	$selectedRole = $model->roleaccess;
-
-$selectedLanguage = Yii::app()->language;
-if($model->languageid !== null)
-	$selectedLanguage = $model->languageid;
-
 
 $form = $this->beginWidget('bootstrap.widgets.BsActiveForm', array(
    	'layout' => BsHtml::FORM_LAYOUT_VERTICAL,
@@ -32,33 +21,12 @@ $form = $this->beginWidget('bootstrap.widgets.BsActiveForm', array(
 		'onkeypress'=>" if(event.keyCode == 13){ submitForm('modal', 'content-form', '". $url ."'); } ",
 	),
 ));
-$model->oldLabel = $model->label;
 ?>
 	<div class="row">
 		<div class="col-sm-6">
 			<?php echo $form->textFieldControlGroup($model, 'label', array('labelOptions'=>array('class'=>'control-label required'), 'controlOptions'=>array('class'=>''))); ?>
-			<?php echo $form->hiddenField($model, 'oldLabel')?>
 		</div>
 	</div>
 	
-	<div class="row">
-		<div class="col-sm-6">
-			<?php
-				echo $form->dropDownListControlGroup($model, 'roleaccess', $roles, array(
-    				'options' => array($selectedRole=>array('selected'=>true)),
-					'labelOptions'=>array('class'=>'control-label required'),
-					'controlOptions'=>array('class'=>''),
-				));
-			?>
-		</div>
-		<div class="col-sm-6">
-			<?php
-				echo $form->dropDownListControlGroup($model, 'languageid', $languages, array(
-    				'options' => array($selectedLanguage=>array('selected'=>true)),
-					'labelOptions'=>array('class'=>'control-label required'),
-					'controlOptions'=>array('class'=>''),
-				));
-			?>
-		</div>
-	</div>
+	
 <?php $this->endWidget(); ?>
--- protected/views/gallery/_galleryImage.php
+++ protected/views/gallery/_galleryImage.php
@@ -7,6 +7,6 @@
 ?>
 
 <div class="img-thumbnail col-xs-6 col-sm-4 col-md-3 col-lg-2" style="border: none;">
-	<?php echo BsHtml::imageThumbnail($gallery->url,$gallery->head, array('style'=>'width: 100%; height: auto;'));//, array('style'=>'width: 100px; height: auto;') ?>
+	<?php echo BsHtml::imageThumbnail($gallery->url,$gallery->head, array('style'=>'width: 100%; height: auto;')); ?>
 	<span><?php echo $gallery->head ?></span>
 </div>
